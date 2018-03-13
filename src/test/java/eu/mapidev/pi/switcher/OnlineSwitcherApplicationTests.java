04067676c33d76e078fcfaa0985d4e0965badf68
Mark <mpietryga93@gmail.com>
2018-03-13 17:40:49 +0100

    Add initial class and params

---
+++ src/test/java/eu/mapidev/pi/switcher/OnlineSwitcherApplicationTests.java
@@ -0,0 +1,16 @@
+package eu.mapidev.pi.switcher;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OnlineSwitcherApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
